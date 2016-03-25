d3bc0b5e04ef22040f4aa97223b37934e4b576b1
Stavros Kounis <skounis@gmail.com>
2016-03-26 00:54:31 +0200

    feat(grunt) Add hint task

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['src/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+  
+};
