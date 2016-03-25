6d899ed8fc19a9a2a9b56e59adf0f9133e978dd0
Stavros Kounis <skounis@gmail.com>
2016-03-26 00:55:40 +0200

    feat(grunt) Add watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,16 @@ module.exports = function(grunt) {
           jQuery: true
         }
       }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint']);
-  
+
 };
